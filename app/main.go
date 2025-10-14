1893a6130874ba8592c73a6e5abc8d071e8a11d6
Susi Sandoval <sfsandoval@csuchico.edu>
2025-10-14 09:03:12 -0700

    Enable dynamic port binding for Docker container compatibility

--- app/main.go
+++ app/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
+	/*
+		app := fiber.New()
+
+		app.Get("/", func(c *fiber.Ctx) error {
+			response := fiber.Map{
+				"message":   "My name is Susi Sandoval",
+				"timestamp": time.Now().Unix(),
+			}
+			return c.JSON(response)
+		})
+
+		app.Listen(":3000")
+
+	*/
+
+	// Added dynamic port binding to align with Docker's PORT environment variable
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		response := fiber.Map{
+		return c.JSON(fiber.Map{
 			"message":   "My name is Susi Sandoval",
 			"timestamp": time.Now().Unix(),
-		}
-		return c.JSON(response)
+		})
 	})
 
-	app.Listen(":3000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	app.Listen(":" + port)
+
 }
